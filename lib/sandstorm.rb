cc54aa614c80d566fa11370c2d6efb92f028dac6
Ali Graham <ali.graham@bulletproof.net>
2013-11-18 14:44:12 +1030

    redis connections stored thread & fiber-local

--- lib/sandstorm.rb
+++ lib/sandstorm.rb
@@ -2,27 +2,34 @@ require "sandstorm/version"
 
 module Sandstorm
 
-  ATTRIBUTE_TYPES  = {:string              => [String],
-                      :integer             => [Integer],
-                      :float               => [Float],
-                      :id                  => [String],
-                      :timestamp           => [Integer, Time, DateTime],
-                      :boolean             => [TrueClass, FalseClass]
+  ATTRIBUTE_TYPES  = {:string     => [String],
+                      :integer    => [Integer],
+                      :float      => [Float],
+                      :id         => [String],
+                      :timestamp  => [Integer, Time, DateTime],
+                      :boolean    => [TrueClass, FalseClass],
                      }
 
-  COLLECTION_TYPES = {:list                => [Enumerable],
-                      :set                 => [Set],
-                      :hash                => [Hash],
+  COLLECTION_TYPES = {:list       => [Enumerable],
+                      :set        => [Set],
+                      :hash       => [Hash],
                      }
 
   ALL_TYPES = ATTRIBUTE_TYPES.merge(COLLECTION_TYPES)
 
   class << self
-    attr_accessor :redis
-  end
+    def valid_type?(type)
+      ALL_TYPES.keys.include?(type)
+    end
 
-  def self.valid_type?(type)
-    ALL_TYPES.keys.include?(type)
+    # Thread and fiber-local
+    def redis
+      Thread.current[:sandstorm_redis]
+    end
+
+    def redis=(connection)
+      Thread.current[:sandstorm_redis] = connection
+    end
   end
 
 end
