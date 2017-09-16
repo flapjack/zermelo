a06ad625bc8b6bfc15664a09030dfac46222b33b
Ali Graham <ali.graham@gmail.com>
2017-09-16 20:06:55 +0200

    some refactoring of SortStep#resolve

--- lib/zermelo/filters/steps/sort_step.rb
+++ lib/zermelo/filters/steps/sort_step.rb
@@ -24,19 +24,7 @@ module Zermelo
             temp_keys << dest_list
             r_dest_list = backend.key_to_backend_key(dest_list)
 
-            # TODO: raise error in step construction if keys not
-            # passed as expected below
-            sort_attrs_and_orders =
-              case options[:keys]
-              when String, Symbol
-                { options[:keys].to_s => options[:desc].is_a?(TrueClass) ? :desc : :asc }
-              when Array
-                options[:keys].each_with_object({}) do |k, memo|
-                  memo[k.to_sym] = (options[:desc].is_a?(TrueClass) ? :desc : :asc)
-                end
-              when Hash
-                options[:keys]
-              end
+            sort_attrs_and_orders = attrs_and_orders(keys: options[:keys], desc: options[:desc])
 
             # TODO: check if complex attribute types or associations
             # can be used for sorting
@@ -54,38 +42,21 @@ module Zermelo
             class_key = associated_class.send(:class_key)
 
             sort_attrs_and_orders.keys.reverse.each_with_index do |sort_attr, idx|
-              order = sort_attrs_and_orders[sort_attr]
+              sort_opts = { store: r_dest_list }
 
-              sort_opts = {}
+              sort_opts.update(options_order(sort_attr_type: attr_types[sort_attr.to_sym],
+                                             order: sort_attrs_and_orders[sort_attr]))
 
               unless 'id'.eql?(sort_attr.to_s)
                 sort_opts.update(by: "#{class_key}:*:attrs->#{sort_attr}")
               end
 
               if (idx + 1) == sort_attrs_and_orders.size
-                # only apply offset & limit on the last sort
-                o = options[:offset]
-                l = options[:limit]
-
-                if !(l.nil? && o.nil?)
-                  o = o.nil? ? 0 : o.to_i
-                  l = l.nil? || (l.to_i < 1) ? (Zermelo.redis.llen(dest_list) - o) : l
-                  sort_opts.update(limit: [o, l])
-                end
+                sort_opts.update(options_position(dest_list: dest_list,
+                                                  offset: options[:offset],
+                                                  limit: options[:limit]))
               end
 
-              order_parts = []
-              sort_attr_type = attr_types[sort_attr.to_sym]
-              unless %i[integer float timestamp].include?(sort_attr_type)
-                order_parts << 'alpha'
-              end
-              order_parts << 'desc' if 'desc'.eql?(order.to_s)
-
-              unless order_parts.empty?
-                sort_opts.update(order: order_parts.join(' '))
-              end
-
-              sort_opts.update(store: r_dest_list)
               Zermelo.redis.sort(r_dest_list, sort_opts)
             end
 
@@ -96,6 +67,40 @@ module Zermelo
               call(*([r_dest_list] + opts[:shortcut_args]))
           end
         end
+
+        private
+
+          # TODO: raise error in step construction if keys not
+          # passed as expected below
+          def attrs_and_orders(keys:, desc:)
+            case keys
+            when String, Symbol
+              { keys.to_s => desc.is_a?(TrueClass) ? :desc : :asc }
+            when Array
+              keys.each_with_object({}) do |k, memo|
+                memo[k.to_sym] = (desc.is_a?(TrueClass) ? :desc : :asc)
+              end
+            when Hash
+              keys
+            end
+          end
+
+          def options_position(dest_list:, offset:, limit:)
+            # only apply offset & limit on the last sort
+            return {} if limit.nil? && offset.nil?
+            offset = offset.nil? ? 0 : offset.to_i
+            limit = limit.nil? || (limit.to_i < 1) ? (Zermelo.redis.llen(dest_list) - offset) : limit
+            { limit: [offset, limit] }
+          end
+
+          def options_order(sort_attr_type:, order:)
+            order_parts = []
+            order_parts << 'alpha' unless %i[integer float timestamp].include?(sort_attr_type)
+            order_parts << 'desc' if 'desc'.eql?(order.to_s)
+
+            return {} if order_parts.empty?
+            { order: order_parts.join(' ') }
+          end
       end
     end
   end
